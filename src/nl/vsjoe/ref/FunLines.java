01ded9cf406e104a083d223f4dbea4db83485f56
DJFLuFFy vs joe <info@vsjoe.nl>
2014-01-13 18:49:41 +0100

    this class is the source of the random Fun command
    The class is only a public static final array and the fun command takes
    a random string from it everytime its called.

---
+++ src/nl/vsjoe/ref/FunLines.java
@@ -0,0 +1,33 @@
+package nl.vsjoe.ref;
+
+import org.jibble.pircbot.Colors;
+
+public class FunLines {
+	public static final String[] FUNNYLINES = {
+			Colors.OLIVE + "Het is Vandaag Rossje's " + Colors.MAGENTA + "VERJAARDAG!!!",
+			Colors.GREEN + "Chum " + Colors.YELLOW + "is " + Colors.MAGENTA + "Fum!!!",
+			"I like Trains" + Colors.GREEN + " Choo Choo!!!",
+			"Als je echt fun wilt moet je eens " + Colors.OLIVE + "/pong " + Colors.NORMAL + "intypen.",
+			"Als vliegen vliegen achterna vliegen, vliegen vliegen vliegen achterna.",
+			"Van examens krijg je uitslag.",
+			"Fouten zijn kansen om iets te leren.",
+			"Je hoeft niet aan een boom te hangen om een eikel te zijn.",
+			"Geld maakt niet gelukkig, maar gelukkig maken ze geld.",
+			"Het leven is als een neus. Je moet eruit halen wat erin zit.",
+			"Het is lastig om in minecraft met mes en vork te eten.",
+			".fact",
+			"Drink jij thuis ook melk uit een emmer?",
+			"I like " + Colors.MAGENTA + "pudding!!!",
+			"Huh? moest ik iets doen nu??",
+			"[- - ] I'm BATMAN!!!",
+			"Carlos!!!" + Colors.OLIVE + " Fix it!!!",
+			"^ ^",
+			"\\o/",
+			Colors.GREEN + "Meloenen " + Colors.YELLOW + "Meloenen " + Colors.GREEN + "Meloenen " + Colors.YELLOW + "Meloenen " + Colors.GREEN + "Meloenen " + Colors.YELLOW + "Meloenen " + Colors.GREEN + "Meloenen " + Colors.YELLOW + "Meloenen " + Colors.GREEN + "Meloenen " + Colors.YELLOW + "Meloenen " + Colors.GREEN + "Meloenen " + Colors.YELLOW + "Meloenen ",
+			Colors.RED + "LET OP!!!" + Colors.NORMAL + " Deze server kan loslopende koala's bevatten.",
+			"LAGG in de chat typen is net zo zinloos als " + Colors.GREEN + "GROEN!!! " + Colors.NORMAL + "roepen bij Lingo.",
+			Colors.MAGENTA + "Fristy " + Colors.NORMAL + "is alleen voor grote jongetjes.",
+			"Een filantroop is gewoon iemand die veel oude troep wegeeft en doet alsof het veel waard is.",
+			"Wie de pot uit de boom staat te kijken valt niet zelf met de deur in huis.",
+	};
+}
